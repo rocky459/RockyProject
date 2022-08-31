c697f31b52726e4a2216e4c5ad59e6a517a09ed7
NAIDU DOKKALA <NAIDU DOKKALA@MSI>
2022-08-31 22:12:48 +0530

    First..

---
+++ src/rk/Demo1.java
@@ -0,0 +1,17 @@
+package rk;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int marks=50;
+		if (marks>=35)
+	 {
+			System.out.println("Pass="+marks);
+	 }
+		else
+		{
+			System.out.println("Fail ="+marks);
+		}
+	}
+
+}
